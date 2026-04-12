a2de401b08a1d077fe15daff22b93d6f80e71d5f
tavcoder <tarteagadesign@gmail.com>
2026-04-12 13:17:37 +0200

    feat(feed-basis): add SelectButton component

--- src/pages/DevPage.jsx
+++ src/pages/DevPage.jsx
@@ -1,4 +1,6 @@
 // pages/DevPage.jsx
+import { Globe, Users } from "lucide-react";
+import { SelectButton } from "../components/ui/selectButton/SelectButton.jsx"
 import { StoriesBar } from "../components/organisms/storiesBar/StoriesBar.jsx"
 import { PostFeed } from "../components/organisms/postFeed/PostFeed.jsx"
 import { useCurrentUser } from '../hooks/useCurrentUser.js'
@@ -8,9 +10,11 @@ import { useFollowingList } from "../hooks/useFollowingList.js"
 
 export function DevPage() {
     const { currentUser } = useCurrentUser();
-    const { posts, isLoading, hasMore, loadMore } = usePosts();
     const { users, handleStorySeen } = useFollowingList();
-
+    const selectOptions = [
+        { value: "public", label: "Public", icon: <Globe /> },
+        { value: "friends", label: "Best friends", icon: <Users /> }
+    ];
     return (
         <div style={{ padding: '2rem', display: 'flex', flexDirection: 'column', gap: '2rem' }}>
             <StoriesBar
@@ -19,11 +23,10 @@ export function DevPage() {
                 onStorySeen={handleStorySeen}
             />
 
-            <PostFeed
-                posts={posts}
-                isLoading={isLoading}
-                hasMore={hasMore}
-                onLoadMore={loadMore}
+            <SelectButton
+                label="Privacy"
+                options={selectOptions}
+                onChange={(value) => console.log(value)}
             />
         </div>
     )
