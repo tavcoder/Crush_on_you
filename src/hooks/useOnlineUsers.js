eb74f70c599935dc7283599d9338ce8d9d371a9f
tavcoder <tarteagadesign@gmail.com>
2026-06-02 21:06:55 +0200

    feat(hooks): add useOnlineUsers with online users filtering

---
+++ src/hooks/useOnlineUsers.js
@@ -0,0 +1,19 @@
+// hooks/useOnlineUsers.js
+import { useMemo } from 'react'
+import { useFollowing } from './useFollowing'
+
+export function useOnlineUsers() {
+    const { following, isLoading, isError, error } = useFollowing()
+
+    const onlineUsers = useMemo(() =>
+        following?.filter(user => user.isOnline),
+        [following]
+    )
+console.log(following)
+    return {
+        onlineUsers,
+        isLoading,
+        isError,
+        error,
+    }
+}
