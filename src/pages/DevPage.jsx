9cb14e4ac92acc327f9ffb28988a486eacb25d87
tavcoder <tarteagadesign@gmail.com>
2026-06-02 21:20:37 +0200

    feat(DevPage): integrate new sidebar components

--- src/pages/DevPage.jsx
+++ src/pages/DevPage.jsx
@@ -1,26 +1,30 @@
 // pages/DevPage.jsx
+import { useState } from "react"
 import { StoriesBar } from "../components/organisms/storiesBar/StoriesBar.jsx"
 import { CreatePost } from "../components/organisms/createPost/CreatePost.jsx";
 import { PostFeed } from "../components/organisms/postFeed/PostFeed.jsx"
 import { NavBar } from "../components/molecules/navBar/NavBar.jsx"
 import { BottomNav } from "../components/molecules/bottomNav/BottomNav.jsx"
 import { SelectButton } from "../components/ui/selectButton/SelectButton.jsx"
-import { SideBar } from "../components/molecules/sideBar/SideBar.jsx"
+import { LeftSideBar } from "../components/molecules/leftSideBar/LeftSideBar.jsx"
+import { RightSideBar } from "../components/molecules/rightSideBar/RightSideBar.jsx"
 import { useSearch } from '../hooks/useSearch.js'
 import { useCurrentUser } from '../hooks/useUsers.js'
-import { usePosts } from "../hooks/usePosts.js"
+import { usePosts, useUserPosts } from "../hooks/usePosts.js"
 import { useStories } from "../hooks/useStories.js"
 
 
 // DevPage.jsx
 export function DevPage() {
+    const [selectedUserId, setSelectedUserId] = useState(null)
     const { results, isLoading: isSearchLoading, isSearching, query } = useSearch('posts')
     const { data: currentUser, loading: currentUserLoading } = useCurrentUser()
     const { posts, isLoading, addPost } = usePosts()
+    const { data: userPosts, isLoading: userPostLoading, } = useUserPosts()
     const { stories, onStorySeen } = useStories()
     // Si hay búsqueda activa, muestra resultados — si no, muestra el feed normal
-    const displayPosts = isSearching ? results : posts
-    const displayLoading = isSearching ? isSearchLoading : isLoading
+    const displayPosts = isSearching ? results : selectedUserId ? userPosts : posts
+    const displayLoading = isSearching ? isSearchLoading : userPostLoading ? userPostLoading : isLoading
     return (<>
         <NavBar user={currentUser} />
         <StoriesBar
@@ -29,7 +33,7 @@ export function DevPage() {
             onStorySeen={onStorySeen}
         />
         <div style={{ padding: '2rem', display: 'flex', flexDirection: 'row', gap: '2rem' }}>
-            <SideBar user={currentUser} isLoading={currentUserLoading} />
+            <LeftSideBar user={selectedUserId} isLoading={currentUserLoading} />
             <div style={{ padding: '2rem', display: 'flex', flexDirection: 'column', gap: '2rem' }}>
 
                 <CreatePost user={currentUser} onPostCreated={addPost} />
@@ -41,9 +45,13 @@ export function DevPage() {
                 <PostFeed
                     posts={displayPosts}
                     isLoading={displayLoading}
+
                 />
             </div>
+            <RightSideBar onUserClick={setSelectedUserId} />
+
         </div>
+
         <BottomNav />
     </>)
 }
