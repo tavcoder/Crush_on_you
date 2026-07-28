d22de6c145b3929f6414efcbae169ca5c566c693
Tania Arteaga <tarteagadesign@gmail.com>
2026-07-28 13:02:41 +0200

    ix(db): move MongoDB URI to environment variable
    connection.js had the Atlas connection string, including username
    and password, hardcoded in plain text. Now reads from MONGO_URI,
    documented (without the real value) in .env.example.

--- api-rest-red-social/database/connection.js
+++ api-rest-red-social/database/connection.js
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose");
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const connection = async () => {
-
     try {
-        await mongoose.connect("mongodb+srv://admin:!123Friends@cluster0.hpqftkg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Conectado correctamente a bd: mi_redsocial");
-
     } catch (error) {
         console.log(error);
         throw new Error("No se ha podido conectar a la base de datos !!");
     }
-
 }
 
 module.exports = connection;
